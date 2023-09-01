2b7b97e4cb939af511399f73a1a63e6e3615d8aa
nouran <no0ouranmohamed@gmail.com>
2023-09-01 16:16:16 +0300

    updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,24 @@
 #include "main.h"
-unsigned int _strspn(char *s, char *accept){
+/**
+ * _strspn - returns the number of bytes in the initial segment
+ * @s: buffer which is checked
+ * @accept: buffer which we check for its content
+ * Return: i
+ */
+unsigned int _strspn(char *s, char *accept)
+{
 	unsigned int i;
 	int n;
-	for (i = 0; s[i] != '\0'; i++){
-		for (n = 0; accept[n] != '\0'; n++){
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
 			if (s[i] == accept[n])
 				break;
 		}
-		if (s[i] != accept [n])
+		if (s[i] != accept[n])
 			break;
 	}
-	return i;
+	return (i);
 }
