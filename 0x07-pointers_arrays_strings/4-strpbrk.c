b1291bf41a956540cfdf0fb7528d10cd9a214bf4
nouran <no0ouranmohamed@gmail.com>
2023-09-01 16:23:12 +0300

    updated

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,16 +1,27 @@
 #include "main.h"
-char *_strpbrk(char *s, char *accept){
+/**
+ * _strpbrk - locates the first occurrence in the string
+ * @s: pointer to be returned
+ * @accept: we check for characters in it
+ * Return: *s
+ */
+char *_strpbrk(char *s, char *accept)
+{
 	int i, n;
-	for (i = 0; s[i] != '\0'; i++){
-		for (n = 0; accept [n] != '\0'; n++){
-			if (s[i] == accept[n]){
-				return &s[i];
-				break;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+			{
+				return (&s[i]);
 			}
 		}
 	}
-	if (s[i] != accept [n]){
-		return &s[i];
+	if (s[i] == accept[n])
+	{
+		return (&s[i]);
 	}
-	return &s[i];
+	return (0);
 }
