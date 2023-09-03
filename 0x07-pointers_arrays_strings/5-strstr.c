c0406d69f5911adb9139da00373d9efe47272200
nouran <no0ouranmohamed@gmail.com>
2023-09-03 20:04:53 +0300

    update

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - finds a substring in a string
+ * @haystack: string
+ * @needle: substring to be found
+ * Return: pointer
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		while (*needle != '\0')
+		{
+			if (*haystack == *needle)
+				return (haystack);
+		}
+		if (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		else
+			haystack++;
+	}
+	return (0);
+}
