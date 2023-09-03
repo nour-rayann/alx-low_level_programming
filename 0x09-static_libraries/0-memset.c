5f20529aa6215a163e6f65e4b06664a324168c00
nouran <no0ouranmohamed@gmail.com>
2023-09-03 16:28:48 +0300

    update

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,2 +1,20 @@
 #include "main.h"
-char *_memset(char *s, char b, unsigned int n);
+/**
+ *  _memset - function to be executed
+ *  @s: pointer to characters
+ *  @b: character to be printed
+ *  @n: number of bytes
+ *  Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *ptr = s;
+
+	for (i = 0; i < n; i++)
+	{
+		*ptr = b;
+		ptr++;
+	}
+	return (s);
+	}
