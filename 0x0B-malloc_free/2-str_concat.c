d3e48dfe2d1ce216fb3bc02861c846723b32b874
nouran <no0ouranmohamed@gmail.com>
2023-09-06 22:17:54 +0300

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,16 +12,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 != NULL && s2 != NULL)
 	{
-		if (s1 != NULL)
-		{
-			for (i = 0; s1[i] != '\0'; i++)
-			;
-		}
-		if (s2 != NULL)
-		{
-			for (n = 0; s2[n] != '\0'; n++)
-			;
-		}
+		for (i = 0; s1[i] != '\0'; i++)
+		;
+		for (n = 0; s2[n] != '\0'; n++)
+		;
 		sum = n + i;
 		ptr = (char *) malloc((sum + 1) * sizeof(char));
 
