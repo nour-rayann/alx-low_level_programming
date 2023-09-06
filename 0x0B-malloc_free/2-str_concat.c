e607316084b1fc5cd34a88ffda45c5715dd6982b
nouran <no0ouranmohamed@gmail.com>
2023-09-06 22:49:17 +0300

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,43 +10,41 @@ char *str_concat(char *s1, char *s2)
 	int i, n, sum, m, b;
 	char *ptr;
 
-	if (s1 != NULL || s2 != NULL)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		;
-		for (n = 0; s2[n] != '\0'; n++)
-		;
+		if (s1 != NULL)
+		{
+			for (i = 0; s1[i] != '\0'; i++)
+			;
+		}
+		else
+		{
+			s1 = "";
+			i = 1;
+		}
+		if (s2 != NULL)
+		{
+			for (n = 0; s2[n] != '\0'; n++)
+			;
+		}
+		else
+		{
+			s2 = "";
+			n = 1;
+		}
 		sum = n + i;
 		ptr = (char *) malloc((sum + 1) * sizeof(char));
 
 		if (ptr != NULL)
 		{
-			if (s1 != NULL)
-				{
-				for (m = 0; s1[m] != '\0'; m++)
-					ptr[m] = s1[m];
-				}
-			if (s2 != NULL)
+			for (m = 0; s1[m] != '\0'; m++)
+				ptr[m] = s1[m];
+			for (b = 0; s2[b] != '\0'; b++)
 			{
-				for (b = 0; s2[b] != '\0'; b++)
-				{
-					ptr[m] = s2[b];
-					m++;
-				}
+				ptr[m] = s2[b];
 				m++;
 			}
+			m++;
 			ptr[m] = '\0';
 			return (ptr);
 		}
-	}
-	if (s1 == NULL && s2 == NULL)
-	{
-		s1 = "";
-		s2 = "";
-		ptr = (char *) malloc(2);
-		ptr[0] = *s1;
-		ptr[1] = *s2;
-		return (ptr);
-	}
 	return (NULL);
 }
