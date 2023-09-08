38a8609523cb6e71eaccabe11e690fa5188e4822
nouran <no0ouranmohamed@gmail.com>
2023-09-08 20:37:20 +0300

    updated

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,10 +9,11 @@ void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height > 0)
 	{
-		while(height >= 0)
+		int i;
+
+		for (i = 0; i < height; i++)
 		{
-			height--;
-			free(grid[height]);
+			free(grid[i]);
 		}
 		free(grid);
 	}
