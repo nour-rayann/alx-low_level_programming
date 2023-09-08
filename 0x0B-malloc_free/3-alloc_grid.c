d7ebea81b3e3c49e3b942654d58f7104a629badc
nouran <no0ouranmohamed@gmail.com>
2023-09-08 19:32:43 +0300

    updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 
 	if (width > 0 && height > 0)
 	{
-		ptr = malloc(height * sizeof(int));
+		ptr = malloc(height * sizeof(int *));
 
 		if (ptr != NULL)
 		{
