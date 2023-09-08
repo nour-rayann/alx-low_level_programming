5001d5db56388257b0d09aba2bb449121d82c53e
nouran <no0ouranmohamed@gmail.com>
2023-09-08 19:10:24 +0300

    updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,25 +6,26 @@
  * Return: pointer to pointer
 */
 int **alloc_grid(int width, int height)
-{	
+{
 	int i, n;
 	int **ptr;
 
 	if (width > 0 && height > 0)
 	{
 		ptr = malloc(height * sizeof(int));
-		
+
 		if (ptr != NULL)
 		{
 			for (i = 0; i < height; i++)
 			{
-				ptr[i] = malloc(width *sizeof(int));
+				ptr[i] = malloc(width * sizeof(int));
 				for (n = 0; n < width; n++)
 				{
 					ptr[i][n] = 0;
 				}
 			}
 			return (ptr);
+			free(ptr);
 		}
 	}
 	return (NULL);
