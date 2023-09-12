8b4af31798bab6112dd98fbef4b3997697fde248
nouran <no0ouranmohamed@gmail.com>
2023-09-12 19:38:45 +0300

    updated

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - prints info of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
