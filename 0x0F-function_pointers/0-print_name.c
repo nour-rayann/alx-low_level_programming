2eda2e82c62c6a4f05d426cf7c17214a296f7277
nouran <no0ouranmohamed@gmail.com>
2023-09-13 17:47:12 +0300

    updated

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints the name that the user provides
+ * @name: name provided
+ * @f: pointer to a function that prints name
+ * Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
