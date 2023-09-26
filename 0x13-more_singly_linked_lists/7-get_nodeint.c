8fccdfda559e2d1718fae27842687eed95d6d1d8
Nouran <no0ouranmohamed@gmail.com>
2023-09-27 02:33:22 +0300

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the nth node
+ * @head: list
+ * @index: number of node to be returned
+ * Returned: node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node = head;
+	unsigned int count = 0;
+
+	while (node)
+	{
+		if (count == index)
+			return node;
+		node = node->next;
+		count++;
+	}
+	return (NULL);
+}
