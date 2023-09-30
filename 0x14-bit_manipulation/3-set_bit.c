76731fea4b757d2ff10b2ce01fb7be257e26ce74
Nouran <no0ouranmohamed@gmail.com>
2023-10-01 02:42:31 +0300

    fixed

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bit;
 
-	if (index > 64)
+	if (index > 63)
 		return (-1);
 	bit = 1 << index;
 	*n = *n | bit;
-	return (*n);
+	return (1);
 }
